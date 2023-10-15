0ff3615c62b4848615048a925ccb8e426fc10a5b
omairZibew <omair@zibew.com>
2023-10-15 15:44:48 +0530

    UPdate comad done

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+
+using HR.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class UpdateLeaveAllocationCommandHandler : IRequestHandler<UpdateLeaveAllocationCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateLeaveAllocationCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            
+
+            var leaveType = await _leaveTypeRepository.Get(request.LeaveTypeDto.Id);
+
+        
+
+            _mapper.Map(request.LeaveTypeDto, leaveType);
+
+            await _leaveTypeRepository.Update(leaveType);
+         
+
+            return Unit.Value;
+        }
+    }
+}
